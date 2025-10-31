17f5db0efc932106a3df376c659203b63bf74406
Qi Qing <learn.qiqing@gmail.com>
2025-10-31 15:01:50 -0400

    completed Contract class with all attributes, a constructor and getters and setters for all fields except total price and monthly payment. Define abstract methods for getTotalPrice() and getMonthlyPayment() that will return a double (computed values).

--- src/main/java/com/pluralsight/dealership/Contract.java
+++ src/main/java/com/pluralsight/dealership/Contract.java
@@ -1,5 +1,59 @@
 package com.pluralsight.dealership;
 
+import com.pluralsight.Vehicle;
+
 public abstract class Contract {
+    private String dateOfContract;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+    private double totalPrice;
+    private double monthlyPayment;
+
+    public Contract(String dateOfContract, String customerName, String customerEmail,
+                    Vehicle vehicleSold, double totalPrice, double monthlyPayment) {
+        this.dateOfContract = dateOfContract;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+        this.totalPrice = totalPrice;
+        this.monthlyPayment = monthlyPayment;
+    }
+
+    public String getDateOfContract() {
+        return dateOfContract;
+    }
+
+    public void setDateOfContract(String dateOfContract) {
+        this.dateOfContract = dateOfContract;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotalPrice();
+
+    public abstract double getMonthlyPayment();
 
 }
